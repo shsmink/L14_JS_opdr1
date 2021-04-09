ef56ce7a9675abf161ae30c0a37182d470e8acb5
shsmink <79309340+shsmink@users.noreply.github.com>
2021-04-09 15:44:12 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+const makePizza = function() {
+    console.log('Kook stukjes kip in 5 á 6 minuten gaar');
+    console.log('Giet de kip af en breng op smaak');
+    console.log('Rol het pizzadeeg uit op een bakplaat en prik hier wat gaatjes in');
+    console.log('Besmeer het deeg met barbecue saus');
+    console.log('Strooi stukjes ui, mozzarella, paprika, kipstukjes en geraspte kaas over het deeg');
+    console.log('Zet de pizza voor circa 15 tot 20 minuten in de oven totdat de randjes mooi bruin zijn geworden');
+}
+
+makePizza();
+makePizza();
+makePizza();
+
+const makeSushi = function() {
+    console.log('Kook sushirijst');
+    console.log('Meng rijst met rijstazijn');
+    console.log('Snijd ingrediënten in reepjes');
+    console.log('Leg norivel met glimmende kant naar beneden op bamboe matje');
+    console.log('Plak met natte handen ongeveer 2/3 van norivel met rijst, druk stevig aan tot een dikte van 1 cm');
+    console.log('Verdeel ingrediënten over rijst, rol het vel dicht en snijd de rol in plakjes');
+}
+
+makeSushi();
+
+makeSushi();
+makeSushi();
+makeSushi();
+makePizza();
+makePizza();
